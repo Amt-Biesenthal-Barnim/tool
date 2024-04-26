ffdd80fefe90f44f7d0aa1f422aacfc61594e816
Amt-Biesenthal-Barnim <148776656+Amt-Biesenthal-Barnim@users.noreply.github.com>
2024-04-26 13:23:53 +0200

    Update data.js

--- examples/zuhause/data.js
+++ examples/zuhause/data.js
@@ -30,6 +30,37 @@ var APP_DATA = {
       },
       "linkHotspots": [],
       "infoHotspots": []
+    },
+     {
+      "id": "1-flur",
+      "name": "Flur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.004155545838081309,
+        "pitch": 0.0780511992240065,
+        "fov": 1.5465028311360536
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
     }
   ],
   "name": "Zuhause",
